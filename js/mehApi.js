a4a3daabcccb53d80b5a06ec69d3fd6f7295e404
Aaron Steimle <aarons@glyphip.com>
2020-05-22 07:03:35 -0400

    fix for something dumb I did

--- js/mehApi.js
+++ js/mehApi.js
@@ -1,42 +1,14 @@
 
 function getEvents(){
     console.log("getEvents");
-    const xhr = new XMLHttpRequest();
-    let url = 'https://elegin.github.io/2020_cpv/'
-    xhr.open('GET', url);
-    xhr.send(); 
-    xhr.onload = function() {
-        if (xhr.status != 200) { // analyze HTTP status of the response
-            console.log(`Error ${xhr.status}: ${xhr.statusText}`); // e.g. 404: Not Found
-        } else { // show the result
-            console.log(xhr)
-        }
-      };
-      xhr.onerror = function() {
-        
-        console.log(`Error ${xhr.status}: ${xhr.statusText}`);
-      };      
-    // axios.get(url).then(response => console.log(response));
-    //  fetch(url,{
-    //     mode: 'no-cors',
-    //     headers: {
-    //         'Content-Type': 'text/html'
-    //     }
-
-    // })
-    // .then((resp) => console.log(resp)) 
-
-    // .then(data => console.log(data));
-    // fetch('https://elegin.github.io/2020_cpv').then(function (response) {
-    //     // The API call was successful!
-    //     console.log(response)
-    //     return response.text();
-    // }).then(function (html) {
-    //     // This is the HTML from our response as a text string
-    //     console.log(html);
-    // }).catch(function (err) {
-    //     // There was an error
-    //     console.warn('Something went wrong.', err);
-    // });
+    let url = 'https://elegin.github.io/2020_cpv/';
+    axios.get(url)
+    .then(response =>  displayEvents(response.data.events))
+    .catch(response =>  console.log(response));
     
+}
+
+
+function displayEvents(data){
+  console.log(data)
 }
