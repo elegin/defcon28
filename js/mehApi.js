f6693b0504e294b5eaab7593a0a873de032d60cb
Aaron Steimle <aarons@glyphip.com>
2020-05-26 15:30:29 -0400

    display json event tracks

--- js/mehApi.js
+++ js/mehApi.js
@@ -3,30 +3,55 @@ function getEvents(){
     let gitPath ="2020_"+window.location.pathname.split("/").pop().split(".")[0]+"/";
     let url = 'https://elegin.github.io/'+gitPath;
     axios.get(url)
-    .then(response =>  displayEvents(response.data.events))
+    .then(response =>  displayEvents(response.data.tracks))
     .catch(response =>  console.log(response));
     
 }
 
 
 function displayEvents(data){
-  console.log(data.length)
   if (data.length > 0){
-    for (i = 0, len = data.length, text = ""; i < len; i++) {
-      console.log(text)
-      text += ` <div class="card">
-                  <div class="container">
-                      <h4><b>${data[i].type}</b></h4>
-                      <p>${data[i].desc}</p>
-                  </div>
-                </div>`
-      // text += `<div class="card">`
-      //   text += `<div class="container">`
-      //     text += `<h4><b>${data[i].type}</b></h4>`
-      //     text += `<p>${data[i].desc}</p>`
-      //   text += `</div">`  
-      // text += `</div">`
-    }
+    //1 track sixteen
+    //2 track eight
+    //3 track one-third
+    let trackGrid = ["sixteen", "eight", "five"]
+    let text = `
+      <div class="section ">
+        <div class="container">`;
+   
+    let tracksColumn = data.length; 
+    let eventsRows = []
+    for (i = 0, len = tracksColumn; i < len; i++) {
+      eventsRows.push(data[i].events.length)
+    } 
+    maxCnt = Math.max.apply(Math, eventsRows)
+    for (i = 0, len = maxCnt; i < len; i++) {
+      text += `<div class="row">`
+      for (j = 0, ght = tracksColumn; j < ght; j++) {
+        if (data[j].events[i] != undefined){
+
+          text += `
+          <div class="${trackGrid[tracksColumn-1]} columns ">
+            <div class="card">
+              <h4><b>${data[j].events[i].type}</b></h4>
+              <p>$${data[j].events[i].desc}</p>
+            </div>
+           </div>          
+          `
+        }else{
+          text += `
+          <div class="${trackGrid[tracksColumn-1]} columns ">
+            <p >&nbsp;</p>
+           </div>
+          `;          
+        }
+      }
+      text += `</div>`;
+    } 
+      text += `</div>`;
+    text += `</div>`;
+
+
     console.log(text)
     document.getElementById("maincontent").innerHTML = text;
   }else{
@@ -34,10 +59,4 @@ function displayEvents(data){
   }
 
 }
-{/* <div class="card">
-  <img src="img_avatar2.png" alt="Avatar" style="width:100%">
-  <div class="container">
-    <h4><b>Jane Doe</b></h4> 
-    <p>Interior Designer</p> 
-  </div>
-</div> */}
+
