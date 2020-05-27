a72e73bb661ac9ede8382f3abb6b04cbda9f21a4
Aaron Steimle <aarons@glyphip.com>
2020-05-26 23:40:16 -0400

    new svg

---
+++ js/common.js
@@ -0,0 +1,12 @@
+function a(id){
+    var ab=document.getElementById(id);
+    ab.style.fill ="green";
+    ab.style.cursor = "pointer";
+}
+function b(id){
+    var ab=document.getElementById(id);
+    ab.style.fill="#f8961d";
+}
+function c(link){
+    window.location.href = link+".html"
+}
